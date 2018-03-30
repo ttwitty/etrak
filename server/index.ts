59a2a95828f74d77b2a40808901b15ec3b31aaef
jizaiwill <jizaiwill@gmail.com>
2018-03-30 13:29:22 -0500

    Styling refresh v1
    Restyled homepage, login/signup, added images, and also added node
    directory.

---
+++ server/index.ts
@@ -0,0 +1,24 @@
+// import { join } from 'path';
+// import * as express from 'express';
+// import { json } from 'body-parser';
+// import * as cookieParser from 'cookie-parser'
+// // import apiRouter from './api';
+// // import stateRouting from './middleware/routing.mw';
+// // import configurePassport from './config/passport'; 
+
+// const clientPath = join(__dirname, '../client');
+// // const app = express();
+
+
+// // app.use(express.static(clientPath));
+// // app.use(cookieParser());
+// // app.use(json());
+
+// // configurePassport(app);
+
+// // app.use('/api', apiRouter);
+// // app.get('*', stateRouting);
+
+// app.listen(process.env.PORT || 3000, () => {
+//     console.log('Listening on Port: 3000');
+// })
